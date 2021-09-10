628589d1127a16cf8399400d9f084d187da7c6cd
cnridley <cnridley1@gmail.com>
2021-09-10 17:26:38 +0000

    added user login and register functionality

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import os
 from flask import Flask, flash, render_template, redirect, request, session, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
+
 if os.path.exists("env.py"):
     import env
 
@@ -17,10 +19,52 @@ mongo = PyMongo(app)
 
 
 @app.route("/")
-@app.route("/index")
+@app.route("/index", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        # to check if the user already exists in the DB. Check mongo DB using the 
+        # find_one function and putting the parament
+        # as what the user inputted into the form using the form.get
+        registered_user = mongo.db.user.find_one({"username": request.form.get("username").lower()})
+
+        if registered_user:
+            #make sure the user password matches that the password hash generated in the register route.
+            if check_password_hash(registered_user['password'], request.form.get('password')):
+                session['user'] = request.form.get('username').lower()
+                flash("Welcome, {}".format(request.form.get('username')))
+                return redirect(url_for('space'))
+            
+            else:
+                flash("USERNAME AND/OR PASSWORD IS INCORRECT")
+                return redirect(url_for('index'))
+        
+        else:
+            flash("USERNAME AND/OR PASSWORD IS INCORRECT")
+            return redirect(url_for('index'))
+    
     return render_template("index.html")
 
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+
+        new_user = {
+            "username": request.form.get("username"),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+
+        mongo.db.user.insert_one(new_user)
+        session["user"] = request.form.get("username").lower()
+        flash("USER CREATED SUCCESSFULLY!")
+        return redirect(url_for('index'))
+        
+    return render_template("register.html")
+
+
+@app.route('/space')
+def space():
+    return render_template("space.html")
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
