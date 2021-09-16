2171b3b1f3512244e6bc21657e20f0c7ff9dac06
cnridley <cnridley1@gmail.com>
2021-09-16 19:54:56 +0000

    added styling to fetched api data

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,6 +1,6 @@
 async function sendApiRequest(){
-    let api_key = "ausYefrhnm9ixSnHn7hClnVkv7YfsUDL4yQMhQSL"
-    let response = await fetch("https://images-api.nasa.gov/search?q=apollo%2011...");
+    let apiKey = "ausYefrhnm9ixSnHn7hClnVkv7YfsUDL4yQMhQSL"
+    let response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=10`);
     console.log(response)
     let data = await response.json()
     console.log(data)
@@ -10,7 +10,34 @@ async function sendApiRequest(){
 sendApiRequest();
 
 function useApiData(data){
-    document.querySelector("#content").innerHTML += data.collection.items[3].data[0].description
-    
+    let card = "";
+
+    for (let d of data){
+        card += `
+        
+            <div class="card col-md-6">
+                <img class="card-img-top" src="${d.url}" alt="Nasa Image">
+                <div class="card-body">
+                  <h5 class="card-title">${d.title}</h5>
+                  <p class="card-text">${d.explanation}</p>
+                  <div class="row m-auto container-fluid">
+                  <div class="col-3 m-auto">
+                    <i class="far fa-heart images"></i>
+                  </div>
+                  <div class="col-3 m-auto">
+                    <i class="far fa-thumbs-up images"></i>
+                  </div>
+                  <div class="col-3 m-auto">
+                    <i class="far fa-thumbs-down images"></i>
+                  </div>
+                  </div>                  
+                </div>
+              </div>
+        `
+    }
+  
+    document.getElementById("content").innerHTML = card;
 }
+
+
 
